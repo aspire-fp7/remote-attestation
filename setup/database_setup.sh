83e6594991995f36ffa9bb9c68f5ad2da0971eb7
babrath <bert.abrath@UGent.be>
2017-10-27 16:46:13 +0200

    Start using MYSQL_ROOT_PASSWORD environment variable.

--- setup/database_setup.sh
+++ setup/database_setup.sh
@@ -42,14 +42,14 @@ if [ -v forceDrop ]; then
 	# Dropping all RA database data
 
 	echo "++++ Dropping database"
-	mysql -u root --password=aspire -h $serverAddress -e "DROP DATABASE $raDbName"
+	mysql -u root -p${MYSQL_ROOT_PASSWORD} -h $serverAddress -e "DROP DATABASE $raDbName"
 	checkError $?
 
 fi
 
 # Try to create database
 echo "++++ Setting up ASPIRE database"
-mysql -u root --password=aspire -h $serverAddress -e "CREATE DATABASE $raDbName" &> /dev/null
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -h $serverAddress -e "CREATE DATABASE $raDbName" &> /dev/null
 if [ $? == 0 ]; then
 
 	# Create database structure
@@ -57,14 +57,14 @@ if [ $? == 0 ]; then
 	for userScript in $(ls $createUserSqlDir)
 	do
 		echo "++++ Creating user for $userScript"
-		mysql -u root --password=aspire -h $serverAddress < $createUserSqlDir/$userScript
+		mysql -u root -p${MYSQL_ROOT_PASSWORD} -h $serverAddress < $createUserSqlDir/$userScript
 		#checkError $? #removed due to a mySQL bug: error when try to create a user already existing
 	done;
 
 	for tablesScript in $(ls $createTablesSqlDir)
 	do
 		echo "++++ Creating tables for $tablesScript"
-		mysql -u root --password=aspire -h $serverAddress $raDbName < $createTablesSqlDir/$tablesScript
+		mysql -u root -p${MYSQL_ROOT_PASSWORD} -h $serverAddress $raDbName < $createTablesSqlDir/$tablesScript
 		checkError $?
 	done
 else
